ea5f45739518c39e62f3ae9207184c1702ac6923
Irfan Rifai <m.irfanrifai@gmail.com>
2022-10-04 21:43:15 +0700

    FLUTTER 29. Menampilkan QR Code

--- lib/pages/21-40/29_menampilkan_qr_code.dart
+++ lib/pages/21-40/29_menampilkan_qr_code.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, library_private_types_in_public_api, prefer_interpolation_to_compose_strings, sized_box_for_whitespace, import_of_legacy_library_into_null_safe
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, library_private_types_in_public_api, prefer_interpolation_to_compose_strings, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
