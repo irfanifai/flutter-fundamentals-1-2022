40cc654bfe2369fb674d00ab012b203e20096368
Irfan Rifai <m.irfanrifai@gmail.com>
2022-10-04 21:50:14 +0700

    FLUTTER 30. Button Belah Ketupat Warna Warni (Transform)

--- lib/pages/21-40/30_button_belah_ketupat_transform.dart
+++ lib/pages/21-40/30_button_belah_ketupat_transform.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, library_private_types_in_public_api, prefer_interpolation_to_compose_strings, sized_box_for_whitespace, import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
-import 'pages/21-40/colorfull_button.dart';
+import 'colorfull_button.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/pages/21-40/colorfull_button.dart
@@ -0,0 +1,116 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, library_private_types_in_public_api, prefer_interpolation_to_compose_strings, sized_box_for_whitespace, import_of_legacy_library_into_null_safe, must_be_immutable, no_logic_in_create_state
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ColorfulButton extends StatefulWidget {
+  Color mainColor, secondColor;
+  IconData iconData;
+  @override
+  _ColorfulButtonState createState() =>
+      _ColorfulButtonState(mainColor, secondColor, iconData);
+
+  ColorfulButton(this.mainColor, this.secondColor, this.iconData);
+}
+
+class _ColorfulButtonState extends State<ColorfulButton> {
+  bool isPressed = false;
+  Color mainColor, secondColor;
+  IconData iconData;
+
+  _ColorfulButtonState(this.mainColor, this.secondColor, this.iconData);
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: pi / 4,
+      child: GestureDetector(
+        onTapDown: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapUp: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapCancel: () {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        child: Material(
+          borderRadius: BorderRadius.circular(15),
+          elevation: (isPressed) ? 5 : 10,
+          shadowColor: (isPressed) ? secondColor : mainColor,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Stack(
+              children: [
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(15),
+                    color: (isPressed) ? secondColor : mainColor,
+                    child: Transform.rotate(
+                      angle: -pi / 4,
+                      child: Icon(
+                        iconData,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
